596d1a2ae6da8af28b8e118569c3fe3f7c2fb423
Mike Wilder <mikewilder01@gmail.com>
2021-02-03 18:57:00 -0500

    commit tree fbb2343ddc9d79626a30631e0ca3de034370e30c

--- app.rb
+++ app.rb
@@ -3,7 +3,31 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
-  end
-end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post "/pirates" do
+      begin
+        @pirate = Pirate.new(params[:pirate])
+
+        params[:pirate][:ships].each do |details|
+          Ship.new(details)
+        end
+      
+        @ships = Ship.all
+
+        erb :'pirates/show'
+      rescue => error
+        binding.pry
+        puts "error"
+        raise error
+      end
+    end
+  end	  
+end	
